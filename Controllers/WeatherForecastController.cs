ebf616f6849de9e4928bc1f11547c5fe860b3d16
sujeewa ediriweera <sujeewa.ediriweera@gmail.com>
2022-12-22 23:57:08 +0530

    testing

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "67","sujeewa"
+        "71","sujeewa"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
